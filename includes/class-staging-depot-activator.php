c05a4d337d3ecbc06f4361d58a6f952d8b422e55
Duane Leem <duane@leemfamily.com>
2019-09-07 10:16:31 -0700

    first commit

---
+++ includes/class-staging-depot-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://duaneleem.com
+ * @since      1.0.0
+ *
+ * @package    Staging_Depot
+ * @subpackage Staging_Depot/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Staging_Depot
+ * @subpackage Staging_Depot/includes
+ * @author     Duane Leem <duane@leemfamily.com>
+ */
+class Staging_Depot_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-staging-depot-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://duaneleem.com
+ * @since      1.0.0
+ *
+ * @package    Staging_Depot
+ * @subpackage Staging_Depot/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Staging_Depot
+ * @subpackage Staging_Depot/includes
+ * @author     Duane Leem <duane@leemfamily.com>
+ */
+class Staging_Depot_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
