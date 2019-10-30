05b1f3a341ea53e7186ce76f6c67cddd6014de79
Gabe Tumbaga <djgtraxx@gmail.com>
2019-10-30 04:07:03 +0000

    finished the headboard functionality.  it seems the add-to-cart for bundles is not working, but it looks like an ezr problem

--- public/ezrentout/class-ezrentout-product-buttons.php
+++ public/ezrentout/class-ezrentout-product-buttons.php
@@ -33,6 +33,7 @@ if (!class_exists("Staging_Depot_EZRentOut_Product_Buttons")) {
         $anchorTag_addToCart = "<a class='single_add_to_cart_button button' style='{$styleCSS}' id='ezr-cart-widget-item-{$ezrID}' onclick='ezrAddItemToCartDialog({$ezrID}, this)' href='#_'>Add To Cart</a>";
         $anchorTag_calendar  = "<a class='single_add_to_cart_button button' style='{$styleCSS}' id='ezr-cart-widget-item-{$ezrID}' onclick='ezrLoadCalendarDialog({$ezrID}, this)' href='#_'>Availability Calendar</a>";
         $anchorTag_details   = "<a class='single_add_to_cart_button button' style='{$styleCSS}' id='ezr-cart-widget-item-{$ezrID}' onclick='ezrShowAssetDetails({$ezrID}, this)' href='#_'>Item Detail</a>";
+        //$anchorTag_calByName = "<a class='single_add_to_cart_button button' style='{$styleCSS}' onclick='ezrLoadCalendarDialog({$ezrID}, this, {availabilityByName: true})' href='#_'>Availability by Name</a>";
         break;
       case "bundle":
         $anchorTag_addToCart = "<a class='single_add_to_cart_button button' style='{$styleCSS}' id='ezr-cart-widget-bundle-{$ezrID}' onclick='ezrAddBundleToCartDialog(\'{$ezrID}-b\', this)' href='#_'>Add To Cart</a>";
@@ -41,11 +42,12 @@ if (!class_exists("Staging_Depot_EZRentOut_Product_Buttons")) {
         break;
       }
       if ($ezrID) {
+        //echo "<div style='margin-bottom: 1em;'>{$anchorTag_addToCart}{$anchorTag_calendar}{$anchorTag_details}{$anchorTag_calByName}</div>";
         echo "<div style='margin-bottom: 1em;'>{$anchorTag_addToCart}{$anchorTag_calendar}{$anchorTag_details}</div>";
       }
 
       //this is just for debug purposes
-      echo "headboard offer: {$headboardOffer}<br/>";
+      //echo "headboard offer: {$headboardOffer}<br/>";
 
       //check if the headboard attribute is set, and if it is, lets get all the products that correspond.
       if ($headboardOffer) {
@@ -53,13 +55,24 @@ if (!class_exists("Staging_Depot_EZRentOut_Product_Buttons")) {
 
         foreach ($headboardOfferedProducts as $hbKey => $hbProduct) {
           $hbP_ezrItemType = $hbProduct->get_meta( 'ezrentout_item_type' );
-          //echo "<pre>" . print_r($hbProduct, true) . "</pre>";
-          //echo "<pre>" . print_r($hbP_ezrItemType, true) . "</pre>";
-          echo "<pre>" . print_r($hbProduct->id, true) . "</pre>";
-          echo "<pre>" . $hbP_ezrItemType . "</pre>";
+          $hbP_ID = $hbProduct->get_meta( 'ezrentout_id' );
+          $hbP_anchorTag_addToCart = "<a class='single_add_to_cart_button button' style='{$styleCSS}' id='ezr-cart-widget-bundle-{$hbP_ID}' onclick='ezrAddBundleToCartDialog(\'{$hbP_ID}-b\', this)' href='#_'>Add {$headboardOffer} To Cart</a>";
+          //this is just for debug purposes
+          //echo "<pre>" . print_r($hbProduct->id, true) . "</pre>";
+          //echo "<pre>" . $hbP_ezrItemType . "</pre>";
+          //echo "<pre>" . $hbP_ID . "</pre>";
         }
-        //
-      }
+        echo "
+          <blockquote title='Headboard Offer'>
+            <small>
+              <p>Based on your selection, unless you have your own boxspring & mattress, you will need to pick \"Bed setup\" based on the size of the headboard.</p>
+              <p>We do NOT use inflatables, as in the past, too many times Stagers have had to return to the property to re-inflate.</p>
+              <p>We use instead a combination of lightweight supports, portable frames, and mattresses.  You just supply the bed sheets.</p>
+            </small>
+          </blockquote>
+          {$hbP_anchorTag_addToCart}
+        ";
+      } //headboardOffer
     } // change_to_ezr_details_button()
 
     public function custom_load_variation_settings_products_fields( $variations ) {
